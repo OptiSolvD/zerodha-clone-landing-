0ebfede58fbba93115d8c8c4d04f64e38df56056
WebAddict-ai <rajkaushik103@gmail.com>
2026-05-22 12:50:52 +0530

    almost done except the correct data feeding

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css';
+import HomePage from './landing_paage/home/HomePage';
+import NotFound from "./landing_paage/NotFound";
+import Navbar from "./landing_paage/Navbar";
+import Footer from "./landing_paage/Footer";
+import Signup from "./landing_paage/signup/Signup";
+import Login from "./landing_paage/login/login";
+import AboutPage from "./landing_paage/about/AboutPage";
+import ProductPage from "./landing_paage/products/ProductPage";
+import PricingPage from "./landing_paage/pricing/PricingPage";
+import SupportPage from "./landing_paage/support/SupportPage";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <BrowserRouter>
+   <Navbar />
+  
+     <Routes>
+       <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/product" element={<ProductPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} />
+    
+       <Route path="*" element={<NotFound />} />
+     </Routes>
+      <Footer />
+   </BrowserRouter>
+);
+
