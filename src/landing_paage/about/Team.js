89b9b0490920cf88ebe34fdce154b8bdede744f7
WebAddict-ai <rajkaushik103@gmail.com>
2026-05-25 12:55:17 +0530

    about section fixed

--- src/landing_paage/about/Team.js
+++ src/landing_paage/about/Team.js
@@ -16,25 +16,88 @@ function Team() {
             style={{ borderRadius: "100%", width: "50%", height: "40%" }}
           />
           <h4 className="mt-5">Anish Raj</h4>
-          <h6>Founder, CEO</h6>
+          <h6>Developer & Entrepreneur</h6>
         </div>
         <div className="col-6 p-3">
+        <p>
+
+  Anish is a visionary tech enthusiast,
+
+  strategic thinker, and developer
+
+  driven by innovation, entrepreneurship,
+
+  and building impactful digital ecosystems.
+
+</p>
+        <p>
+
+  Passionate about modern technologies,
+
+  scalable systems, and ambitious ideas,
+
+  he is currently exploring the intersection
+
+  of AI and software engineering at
+
+  IIT Gandhinagar.
+
+</p>
+          
           <p>
-            Anish bootstrapped and founded Zerodha in 2010 to overcome the
-            hurdles he faced during his decade long stint as a trader. Today,
-            Zerodha has changed the landscape of the Indian broking industry.
-          </p>
-          <p>
-            He is a member of the SEBI Secondary Market Advisory Committee
-            (SMAC) and the Market Data Advisory Committee (MDAC).
-          </p>
-          <p>Playing basketball is his zen.</p>
-          <p>
-            Connect on <a href="">Homepage</a> / <a href="">TradingQnA</a> /{" "}
-            <a href="">Twitter</a>
+            Connect on <a href=""><i class="fa-solid fa-user"></i></a> / <a href="https://www.linkedin.com/in/anish-raj-94a30b370?utm_source=share_via&utm_content=profile&utm_medium=member_android"><i class="fa-brands fa-square-linkedin"></i></a> /{" "}
+            <a href="https://github.com/imVynor"><i class="fa-brands fa-square-github"></i></a>/{" "}
+            <a href="https://www.instagram.com/_.anish.raj/"><i class="fa-brands fa-square-instagram"></i></a>
           </p>
         </div>
       </div>
+
+      <div
+        className="row p-3 text-muted"
+        style={{ lineHeight: "1.8", fontSize: "1.2em" }}
+      >
+       
+        <div className="col-6 p-3">
+          <p>
+
+  Raj is a full stack developer,
+
+  tech enthusiast, and curious builder
+
+  passionate about creating meaningful
+
+  digital products and solving real-world
+
+  problems through technology.
+
+</p>
+          <p>
+
+  Currently exploring AI systems,
+
+  web technologies, and entrepreneurship
+
+  while studying at IIT Gandhinagar.
+
+</p>
+          
+          <p>
+            Connect on <a href=""><i class="fa-solid fa-user"></i></a> / <a href=""><i class="fa-brands fa-square-linkedin"></i></a> /{" "}
+            <a href="https://github.com/OptiSolvD/"><i class="fa-brands fa-square-github"></i></a> /{" "}
+            <a href="https://www.instagram.com/raj720674/"><i class="fa-brands fa-square-instagram"></i></a>
+          </p>
+        </div>
+         <div className="col-6 p-3 text-center">
+          <img
+            src="media/images/raj.jpeg"
+            style={{ borderRadius: "100%", width: "50%", height: "60%" }}
+          />
+          <h4 className="mt-5">Anish Raj</h4>
+          <h6>Developer & Entrepreneur</h6>
+        </div>
+      </div>
+       
+
     </div>
      );
 }
